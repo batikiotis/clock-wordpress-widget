0552f36e4e4b607399dacfd93520dbdf790005b4
Theodoros Drivas <batikiotis@users.noreply.github.com>
2017-06-26 00:27:07 +0300

    Update clock-wordpress-widget.php
    changed update function in order to update time format option

--- clock-wordpress-widget.php
+++ clock-wordpress-widget.php
@@ -43,6 +43,8 @@
   public function update( $new_instance, $old_instance ) {
     $instance = $old_instance;
     $instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+    $instance[ 'tm_format' ] = strip_tags( $new_instance[ 'tm_format' ] );
+
     return $instance;
   }
  }
