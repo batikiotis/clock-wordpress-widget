1ca1e6855fb64f829613c0443477552d63c82b5d
Theodoros Drivas <batikiotis@users.noreply.github.com>
2017-06-27 14:58:15 +0300

    Added some options

--- clock-wordpress-widget.php
+++ clock-wordpress-widget.php
@@ -3,7 +3,7 @@
  * Plugin Name:   Clock Wordpress Widget Plugin
  * Plugin URI:    https://github.com/batikiotis/clock-wordpress-widget
  * Description:   Display time in your frontend wordpress installation.
- * Version:       1.0
+ * Version:       1.1
  * Author:        Drivas Theodoros
  * Author URI:    https://www.yria.com.gr
  */
@@ -17,25 +17,52 @@
   }
     // Create the widget output.
   public function widget( $args, $instance ) {
-    //$tm_format = get_admin_settings('tm_format');
-    $tm_format = apply_filters( 'tm_format', $instance[ 'tm_format' ] );
-    $time = current_time( $tm_format, $gmt = 0 );
-
-    echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title']; ?>
-    <p><strong>Time:</strong> <?php echo $time ?></p>
-   <p><strong>Time format:</strong> <?php echo $tm_format ?></p>
-
+    extract( $args );
+    $title = get_option( 'widget_title', $instance[ 'title' ] );
+    $hour = apply_filters( 'hour', $instance[ 'hour' ] );
+    $amorpm = apply_filters( 'amorpm', $instance[ 'amorpm' ] );
+    $seperator = apply_filters( 'seperator', $instance[ 'seperator' ] );
+    echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title'];
+    ?>
+    <p>
+        <span class='time-clock-hour'><?php echo date($hour, current_time( 'timestamp', 0 ) ); ?></span>
+        <span class='time-clock-seperator'><?php echo $seperator; ?></span>
+        <span class='time-clock-min'> <?php echo date('i', current_time( 'timestamp', 0 ) ); ?></span>
+        <span class='time-clock-meridiem'><?php echo date($amorpm, current_time( 'timestamp', 0 ) ); ?></span>       
+    </p>
     <?php echo $args['after_widget'];
   }
-  
+   
   // Create the admin area widget settings form.
   public function form( $instance ) {
-    $tm_format = !empty( $instance['tm_format'] ) ? $instance['tm_format'] : '';?>
+    $title = ! empty( $instance['title'] ) ? $instance['title'] : '';
+    $seperator =!empty( $instance['seperator']) ? $instance['seperator'] : '';
+    $hour = !empty( $instance['hour']) ? $instance['hour'] : '';
+    $amorpm = !empty( $instance['amorpm'] ) ? $instance['amorpm'] : '';?>
     <p>
-      <label for="<?php echo $this->get_field_id( 'tm_format' ); ?>">PHP Date Format:</label>
-      <input type="text" id="<?php echo $this->get_field_id( 'tm_format' ); ?>" name="<?php echo $this->get_field_name( 'tm_format' ); ?>" value="<?php echo esc_attr( $tm_format ); ?>" />
-    
-    </p><?php
+      <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+      <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+    </p>
+    <p>
+      <label for="<?php echo $this->get_field_id( 'seperator' ); ?>">Seperator:</label>
+      <input type="text" id="<?php echo $this->get_field_id( 'seperator' ); ?>" name="<?php echo $this->get_field_name( 'seperator' ); ?>" value="<?php echo esc_attr( $seperator ); ?>" />
+    </p>
+    <p><label for="<?php echo $this->get_field_id('hour'); ?>">
+        <?php _e('12 hour format:'); ?>
+        <input id="<?php echo $this->get_field_id('h'); ?>" name="<?php echo $this->get_field_name('hour'); ?>" type="radio" value="h" <?php if($hour === 'h'){ echo 'checked="checked"'; } ?> />
+    </label><br>
+    <label for="<?php echo $this->get_field_id('hour'); ?>">
+        <?php _e('24 hour format:'); ?>
+        <input id="<?php echo $this->get_field_id('H'); ?>" name="<?php echo $this->get_field_name('hour'); ?>" type="radio" value="H" <?php if($hour === 'H'){ echo 'checked="checked"'; } ?> />
+    </label></p>
+    <p><label for="<?php echo $this->get_field_id('amorpm'); ?>">
+        <?php _e('Small letters meridiam:'); ?>
+        <input id="<?php echo $this->get_field_id('a'); ?>" name="<?php echo $this->get_field_name('amorpm'); ?>" type="radio" value="a" <?php if($amorpm === 'a'){ echo 'checked="checked"'; } ?> />
+    </label><br>
+    <label for="<?php echo $this->get_field_id('amorpm'); ?>">
+        <?php _e('Capital Meridiam:'); ?>
+        <input id="<?php echo $this->get_field_id('A'); ?>" name="<?php echo $this->get_field_name('amorpm'); ?>" type="radio" value="A" <?php if($amorpm === 'A'){ echo 'checked="checked"'; } ?> />
+    </label></p>   <?php
   }
 
 
@@ -43,8 +70,9 @@
   public function update( $new_instance, $old_instance ) {
     $instance = $old_instance;
     $instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
-    $instance[ 'tm_format' ] = strip_tags( $new_instance[ 'tm_format' ] );
-
+    $instance[ 'hour' ] = strip_tags( $new_instance[ 'hour' ] );
+    $instance[ 'seperator' ] = strip_tags( $new_instance[ 'seperator' ] );
+    $instance[ 'amorpm' ] = strip_tags( $new_instance[ 'amorpm' ] );
     return $instance;
   }
  }
